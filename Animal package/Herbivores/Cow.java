c876fe091c0dc1230151424121e60fa452070368
Janaki Pillai <janakipillai968300@gmail.com>
2024-12-14 22:43:56 +0530

    Create Cow.java

---
+++ Animal package/Herbivores/Cow.java
@@ -0,0 +1,13 @@
+package animalpackage.Herbivores;
+import animalpackage.Animal;
+public class Cow implements Animal
+{
+    public void get_cries()
+    {
+        System.out.println("Cow Cry Type : Moo");
+    }
+    public void get_eating_type()
+    {
+        System.out.println("Cow Eating Type : Herbivores");
+    }
+}
