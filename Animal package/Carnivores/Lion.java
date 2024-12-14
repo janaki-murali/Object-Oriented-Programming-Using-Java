b7a5d27c614bc8d65092525f7d2e77e4b0b5cabe
Janaki Pillai <janakipillai968300@gmail.com>
2024-12-14 22:46:53 +0530

    Create Lion.java

---
+++ Animal package/Carnivores/Lion.java
@@ -0,0 +1,13 @@
+package animalpackage.Carnivores;
+import animalpackage.Animal;
+public class Lion implements Animal 
+{
+    public void get_cries()
+    {
+        System.out.println("Lion Cry Type : Roar");
+    }
+    public void get_eating_type()
+    {
+        System.out.println("Lion Eating Tpe : Carnivores");
+    }
+}
