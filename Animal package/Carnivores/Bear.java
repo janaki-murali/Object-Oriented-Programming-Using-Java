65205662baa4ec2b88c5a36a9ee8a5d47fed6612
Janaki Pillai <janakipillai968300@gmail.com>
2024-12-14 22:47:50 +0530

    Create Bear.java

---
+++ Animal package/Carnivores/Bear.java
@@ -0,0 +1,13 @@
+package animalpackage.Carnivores;
+import animalpackage.Animal;
+public class Bear implements Animal
+{
+    public void get_cries()
+    {
+        System.out.println("Bear Cry Type : Growl");
+    }
+    public void get_eating_type()
+    {
+        System.out.println("Bear Eating Type : Carnivores");
+    }
+}
