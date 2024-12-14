0132dbbc994803569f11149053414b9e781aaf78
Janaki Pillai <janakipillai968300@gmail.com>
2024-12-14 22:45:17 +0530

    Create Elephant.java

---
+++ Animal package/Herbivores/Elephant.java
@@ -0,0 +1,13 @@
+package animalpackage.Herbivores;
+import animalpackage.Animal;
+public class Elephant implements Animal
+{
+    public void get_cries()
+    {
+        System.out.println("Elephant Cry Type : Trumpet");
+    }
+    public void get_eating_type()
+    {
+        System.out.println("Elephant Eating Tpe : Herbivores");
+    }
+}
