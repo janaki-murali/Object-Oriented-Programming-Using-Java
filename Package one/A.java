36ce450f8e5e558f52bad0ad85ebec2152a4903f
Janaki Pillai <janakipillai968300@gmail.com>
2025-01-02 22:54:32 +0530

    Update A.java

--- Package one/A.java
+++ Package one/A.java
@@ -1,3 +1,8 @@
+/*Write a java programme which contain three classes A,B,Main.The class A is in package ‘packageone’ and contain a method
+‘methodclassone’ that prints a message stating that it is printed from class A .Similarly class B is in ‘packagetwo’ and contain a method
+‘methodclasstwo’ that print a message stating that it is printed from class
+B.The Main class import these classes creates their instances and invokes their corresponding methods.*/
+
 package packageone;
 public class A
 {
