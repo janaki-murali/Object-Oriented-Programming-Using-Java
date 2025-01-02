fdfce8c9e38d630c88d83b539f6efe9279d6aa32
Janaki Pillai <janakipillai968300@gmail.com>
2025-01-02 23:47:27 +0530

    Update FileWriterFileReader.java

--- FileWriterFileReader.java
+++ FileWriterFileReader.java
@@ -1,25 +1,34 @@
-import java.io.*;
-public class FileWriterFileReader 
-{
-    public static void main(String args[]) 
-    {
-        try
-        {
-            FileWriter writer = new FileWriter("output.txt");
-            writer.write("Hello\nWe are learning FileWriter and FileReader.\n");
-            writer.close();
-            System.out.println("Successfully wrote to the file.");
-            FileReader reader = new FileReader("output.txt");
-            int character;
-            while ((character = reader.read()) != -1) 
-            {
-                System.out.print((char) character);
-            }
-            reader.close();
-        } 
-        catch (IOException ex) 
-        {
-            System.out.println("error : "+ex);
+/*To write a java program to implement file handling with reader/writer.*/
+
+import java.util.Scanner;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+class FileWrRe {
+    public static void main(String args[]) {
+        try {
+            System.out.print("Enter the text to insert: ");
+            Scanner s = new Scanner(System.in);
+            String str = s.nextLine();
+            FileWriter file = new FileWriter("sample.txt");
+            file.write(str);
+            file.close();
+            System.out.println("\nText written successfully");
+        } catch (Exception e) {
+            System.out.println("\nError occurred while writing to the file");
+            e.printStackTrace();
+        }
+
+        try {
+            char[] ch = new char[100];
+            FileReader rd = new FileReader("sample.txt");
+            int readChars = rd.read(ch);
+            System.out.println("\n\nThe contents in the file are: \n");
+            System.out.println(new String(ch, 0, readChars));
+            rd.close();
+        } catch (Exception e) {
+            System.out.println("\nError occurred while reading the file");
+            e.printStackTrace();
         }
     }
 }
