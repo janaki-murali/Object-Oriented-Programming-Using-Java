637289a6273f5f24c3a6b23cba30275fbfed8017
Janaki Pillai <janakipillai968300@gmail.com>
2025-01-03 18:03:34 +0530

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,28 +1,28 @@
 /*To implement binary search algorihtm using java*/
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Binarysearch {
-  static int ar[]=new int[50];
   static void Binary(int low,int up,int key) {
     int mid;
-    int flag=0,index=0;
-    while (low<=up) {
-      mid=(low+up)/2;
-      if (ar[mid]==key) {
-        flag=1;
-        index = mid+1;
+    int flag = 0,index = 0;
+    while (low <= up) {
+      mid=(low + up)/2;
+      if (ar[mid] == key) {
+        flag = 1;
+        index = mid + 1;
         break;
       }
-      else if (ar[mid]<key) {
-        Low=mid+1;
+      else if (ar[mid] < key) {
+        low = mid + 1;
       }
-      else if (ar[mid]>key) {
-        up=mid-1;
+      else {
+        up = mid - 1;
       }
     }
-    if (flag==1) {
-      System.out.println("Element found at position "+index);
+    if (flag == 1) {
+      System.out.println("Element found at position " + index);
     }
     else {
       System.out.println("Element not found");
@@ -30,17 +30,17 @@ public class Binarysearch {
   }
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int n,l,h;
     System.out.println("Enter the Number of Elements : ");
-    n = sc.nextInt();
-    l=0;h=n-1;
-    System.out.println("Enter "+n+" Elements to the array");
-    for(int i=0;i<n;i++) {
-      ar[i]=sc.nextInt();
+    int n = sc.nextInt();
+    int[] ar = new int[n];
+    System.out.println("Enter "+ n +" Elements to the array : ");
+    for(int i=0; i<n; i++) {
+      ar[i] = sc.nextInt();
     }
+    Array.sort(ar);
     System.out.print("Enter Search Element : ");
     int key = sc.nextInt();
-    Binary(l,h,key);
+    Binary(ar,0,n-1,key);
     sc.close();
   }
 }
